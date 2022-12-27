394ca94cda0e9258749981f7a16d2a01c81e9a26
Binish18 <beenishwaqar1234@gmail.com>
2022-12-27 11:17:32 +0500

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <>
+       <h1 class="text-uppercase" style={{color:"cyan",marginTop:"250px"}}>hello world&nbsp;😄</h1>
+    </>
+  )
+}
+
+export default Hello
